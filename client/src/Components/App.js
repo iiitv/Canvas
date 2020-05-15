5d4c041d0d7d94d930e4eaf3ab6ca2a5f5a341d5
Rohith ch <myselfrohith@outlook.com>
2020-05-15 12:22:37 +0530

    changes added

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -2,13 +2,10 @@ import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./Home/Home";
 import "../scss/main.scss";
+import NavBar from "./Home/NavBaar";
 class App extends Component {
   render() {
-    return (
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
-    );
+    return <NavBar />;
   }
 }
 export default App;
--- client/src/Components/CusButton.jsx
+++ client/src/Components/CusButton.jsx
@@ -1,31 +1,65 @@
 import React, { Component } from "react";
 
 class CusButton extends Component {
+  state = {
+    value: "",
+  };
   dragStart = (e) => {
     const target = e.target;
+    target.classList.add("drag");
+    console.log(target);
     e.dataTransfer.setData("card_id", target.id);
     setTimeout(() => {
       target.style.display = "none";
     }, 0);
   };
+  handleInputChange = (e) => {
+    const currentTarget = e.currentTarget;
+    const value = currentTarget.value;
+    this.setState({ value });
+  };
   dragover = (e) => {
+    const target = e.target;
     e.stopPropagation();
   };
+  getInputStyles = (color, radius) => {
+    console.log("get Input styles" + color);
+    const styles = {};
+    styles.color = `white`;
+    styles.display = "block";
+    styles.borderRadius = `${radius}`;
+    styles.backgroundColor = `${color}`;
+    return styles;
+  };
+  getButtonStyles = () => {
+    const { color, left, top, radius } = this.props;
+
+    const styles = {
+      backgroundColor: color,
+      left: left,
+      top: top,
+      borderRadius: `${radius}px`,
+    };
+    return styles;
+  };
   render() {
+    const { aid, color, radius } = this.props;
     return (
       <button
+        className={aid}
         draggable="true"
         onDragStart={this.dragStart}
         onDragOver={this.dragover}
-        id={this.props.aid}
-        style={{
-          color: this.props.color,
-          left: this.props.left,
-          top: this.props.top,
-          borderRadius: this.props.radius,
-        }}
+        id={aid}
+        style={this.getButtonStyles()}
       >
-        hello
+        <input
+          type="text"
+          style={this.getInputStyles(color, radius)}
+          value={this.state.value}
+          className="buttonInput"
+          onChange={(e) => this.handleInputChange(e)}
+        />
       </button>
     );
   }
